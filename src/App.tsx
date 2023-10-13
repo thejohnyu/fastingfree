537599b023a124d3830fe17d602b91ef92c4791b
John Yu <johnyu.programmer@gmail.com>
2023-10-13 00:07:01 -0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 // import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 import "./App.css";
+import "@aws-amplify/ui-react/styles.css";
+import LeftSideBar from "./components/left-side-bar/LeftSideBar";
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -9,8 +11,14 @@ function App() {
     <>
       <div className="parent">
         <header className="section coral">Header</header>
-        <div className="left-side section blue">Left Sidebar</div>
-        <main className="section green"> Main Content</main>
+        <div className="left-side section blue">
+          <LeftSideBar />
+        </div>
+        <main className="section green">
+          <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="50" cy="50" r="50" />
+          </svg>
+        </main>
         <div className="right-side section yellow">Right Sidebar</div>
         <footer className="section coral">Footer</footer>
       </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      {
+        find: "./runtimeConfig",
+        replacement: "./runtimeConfig.browser", // ensures browser compatible version of AWS JS SDK is used
+      },
+    ],
+  },
+});
