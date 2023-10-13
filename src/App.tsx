c9a68ff8ed1dcf89879ac11bb9c9f4acb32ec167
John Yu <johnyu.programmer@gmail.com>
2023-10-12 23:09:59 -0700

    holy grail layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
